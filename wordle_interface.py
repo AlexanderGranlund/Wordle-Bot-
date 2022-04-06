d20c2276d86ddb68f74bb43f1096be74625008ab
AlexanderGranlund <33897805+AlexanderGranlund@users.noreply.github.com>
2022-04-06 09:49:23 +0200

    Add files via upload

---
+++ wordle_interface.py
@@ -0,0 +1,57 @@
+from wordle_game import game_start_parameters, guess_match_checker, guess_validater, word_list_converter
+
+def menu():
+    print("\n\n\n\nWelcome to Wordle\n")
+    menu_choice = False
+    while menu_choice == False:
+        select = input("Please select one of the the following:\n\nStart new game : Press 1 + Enter\n\nHelp           : Press 2 + Enter\n\n")
+        if select.strip() == "1":
+            menu_choice = True
+            game_start()
+        if select.strip() == "2":
+            menu_choice = True
+            help()
+        else:
+            print("Please select one of the two choices!")
+            go_back = input("Press Enter to continue:") 
+            if go_back == go_back:
+                menu() 
+
+def help():
+    print("Wordle is a word based game where you get six gusses to find the searched five letter word.\nWhen a new game is started you begin by making your first guess,\nthen you will be told if the letters in your guessed word is apart of the searched word and if they are placed correctly relative the searched word.\n")
+    go_back = input("Please press Enter to continue:") 
+    if go_back == "":
+        menu() 
+    else:
+        menu()
+
+def game_start():
+    length_of_words = length_of_word()
+    parameters_dict = game_start_parameters(length_of_words)
+    while parameters_dict["guess_count"] < 6:
+        guess = input("\n\nGive your best guess:\n\n")
+        if guess_validater(guess, length_of_words) == True:
+            parameters_dict["guess_count"] += 1
+            guess_match_checker(parameters_dict["picked_word"], guess)
+        else:
+            print("The Word you guessed isn't in our word list or an uncorrect length")
+            print("Try again and guess a " + str(parameters_dict["length_of_words"]) + " letter word\n\n")
+            go_back = input("Press Enter to continue:")
+    print("\n\n\n\nGame Over!\n\nYou ran out of guesses!\n\nThe correct word was " + str(parameters_dict["picked_word"]))
+    go_back = input("Please press Enter to continue:") 
+    if go_back == "":
+        menu() 
+    else:
+        menu()    
+
+def length_of_word():
+    received_int = False
+    while received_int == False:
+        try:
+            length_of_words = int(input("What length of words do you want to play with?\n\n"))
+            received_int = True
+        except:
+            pass
+    return length_of_words 
+
+menu()
